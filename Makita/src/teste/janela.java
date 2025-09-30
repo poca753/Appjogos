fa00c87c091bce9a7c14ad1a2dab0b9d758022b5
poca753 <165678402+poca753@users.noreply.github.com>
2025-09-30 09:52:29 -0300

    ythth
    tryhetyh

---
+++ Makita/src/teste/janela.java
@@ -0,0 +1,11 @@
+package teste;
+
+import javax.swing.JFrame;
+
+public class janela {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("sony vaio");
+        janela.setBounds(0,0,1920,1080);
+        janela.setVisible(true);
+    }
+}
